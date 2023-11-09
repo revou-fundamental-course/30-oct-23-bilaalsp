fb3e5bc91adaadafa4a128b30edc3a73a0f4a2db
Bilaal Shaafiilana Puja <sounclear08@gmail.com>
2023-11-09 22:29:50 +0700

    second commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function hitungBMI(){
+    const tinggiBadan = document.getElementsByName("tinggiBadan");
+    const beratBadan = document.getElementsByName("beratBadan");
+    const BMI = beratBadan/(tinggiBadan*tinggiBadan)
+}
